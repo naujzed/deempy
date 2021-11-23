693dbe9484e2e44267e7b8aec8be5c32ee8d077f
naujzed <naujzed@gmail.com>
2021-11-23 18:50:36 +0000

    comments

--- dmx.py
+++ dmx.py
@@ -19,48 +19,57 @@ except ImportError:
 
 
 class LogListener:
-	lg = open("downloaded.txt", 'w', encoding='utf-8')
-	flg = open("failed.txt", 'w', encoding='utf-8')
+	
+	# opens files to write successful and failed downloads
+	def __init__(self,failedFile='failedDL.txt',successFile='Downloaded.txt'):
+		self.lg = open(successFile, 'a', encoding='utf-8')
+		self.flg = open(failedFile, 'a', encoding='utf-8')
+		self.failed = 0
 
-	@classmethod
-	def send(cls, key, value=None):
+	# deemix call this function for logging to cli 
+	def send(self, key, value=None):
 		logString = formatListener(key, value)
 		if logString:
 			print(logString)
+		self.writetxt(key,value)
+	
+	# write whether a track downloaded or failed
+	def writetxt(self,key, value=None):
 		if key == "updateQueue":
 			if value.get('downloaded'):
-				cls.lg.write(cls.geturl(value["uuid"])+'\n')
+				self.lg.write(self.geturl(value["uuid"])+'\n')
 			if value.get('failed'):
-				cls.flg.write(cls.geturl(value["uuid"])+'\t' +
-							  value['error']+'\t'+value['errid']+'\n')
-	def geturl(uuid):
+				self.failed +=1
+				self.flg.write(self.geturl(value["uuid"])+'\t' + value['error']+'\t'+value['errid']+'\n')
+				
+	# turns a uuid (<type>_<id>_<bitrate>) to deezer url
+	def geturl(self,uuid):
 		tmp = uuid.split("_")
 		return f"https://www.deezer.com/{tmp[0]}/{tmp[1]}\t{tmp[2]}"
 
 
 class DLR():
-	def __init__(self, portable=None, path=None):
-		localpath = Path('.')
-
-		self.configFolder = localpath / \
-			'config' if portable else localpaths.getConfigFolder()
-		self.settings = loadSettings(self.configFolder)
+	# protable:Bool - is the config folder in the current directory
+	def __init__(self, portable=None):
 		self.dz = Deezer()
 		self.listener = LogListener()
 		self.plugins = {}
+		self.downloadObjects = []
+
+		localpath = Path('.')
+		self.configFolder = localpath / \
+			'config' if portable else localpaths.getConfigFolder()
+		self.settings = loadSettings(self.configFolder)
+
 		if Spotify:
 			self.plugins = {
 				"spotify": Spotify(configFolder=self.configFolder)
 			}
 			self.plugins["spotify"].setup()
-		if path is not None:
-			if path == '':
-				path = '.'
-			path = Path(path)
-			self.settings['downloadLocation'] = str(path)
-		self.downloadObjects = []
+
 		self.findarl()
 
+	# request an arl token until successful login
 	def requestValidArl(self):
 		while True:
 			arl = input("Paste here your arl:")
@@ -68,6 +77,7 @@ class DLR():
 				break
 		return arl
 
+	# find arl in config folder or ask for it
 	def findarl(self):
 		if (self.configFolder / '.arl').is_file():
 			with open(self.configFolder / '.arl', 'r') as f:
@@ -79,16 +89,12 @@ class DLR():
 		with open(self.configFolder / '.arl', 'w') as f:
 			f.write(arl)
 
-	def downloadLinks(self, url, bitrate=None):
+	# turns deezer urls to DL objects stores them in class.downloadObjects
+		# links: list - contains a list of deezer links
+		# bitrate: TrackFormat - a trackformat from deezer.TrackFormats
+	def addToQueue(self, links, bitrate=None):
 		if not bitrate:
 			bitrate = self.settings.get("maxBitrate", TrackFormats.MP3_320)
-		links = []
-		for link in url:
-			if ';' in link:
-				for l in link.split(";"):
-					links.append(l)
-			else:
-				links.append(link)
 
 		for link in links:
 			try:
@@ -102,53 +108,72 @@ class DLR():
 			else:
 				self.downloadObjects.append(downloadObject)
 
+	# starts the download of all downloadObjects in queue then clears it once finished
 	def getsongs(self):
+		sz = len(self.downloadObjects)
+		print(f"DOWNLOADING {sz}")
+
 		for obj in self.downloadObjects:
 			if obj.__type__ == "Convertable":
 				obj = self.plugins[obj.plugin].convert(
 					self.dz, obj, self.settings, self.listener)
-			rs = Downloader(self.dz, obj, self.settings, self.listener).start()
+			Downloader(self.dz, obj, self.settings, self.listener).start()
+		print(f"ALL DONE!: \n\t{self.listener.failed}/{sz} FAILED")
 		self.downloadObjects = []
 
-	def download(self, url, bitrate, path, start=None):
-		# Check for local configFolder
+	# reads in links from file or changes bitrate text to TrackFormat
+		# url: list - can be a list of urls
+		# bitrate: string - choice between 
+			# lossleess:['flac', 'lossless', '9'] 
+			# mp3:['mp3', '320', '3'] ['128', '1'] 
+			# 360:['360', '360_hq', '15'] ['360_mq', '14'] ['360_lq', '13']
+		# filepath: string - the filepath to a txt file of urls
+	def loadLinks(self, url=None, filepath=None,bitrate=None):
 
-		url = list(url)
-		if bitrate:
-			bitrate = getBitrateNumberFromText(bitrate)
-		if path is not None:
-			if path == '':
-				path = '.'
-			path = Path(path)
-			self.settings['downloadLocation'] = str(path)
-		# If first url is filepath readfile and use them as URLs
+		links = []
 		try:
-			isfile = Path(url[0]).is_file()
+			isfile = Path(filepath).is_file()
 		except Exception:
 			isfile = False
 		if isfile:
-			filename = url[0]
-			with open(filename) as f:
-				url = f.readlines()
+			with open(filepath) as f:
+				links = f.readlines()
+		else:
+			if not isinstance(url,list):
+				for link in url:
+					if ';' in link:
+						for l in link.split(";"):
+							links.append(l)
+					else:
+						links.append(link)
+			else:
+				links = url
 
-		self.downloadLinks(url, bitrate)
-		if start:
-			print("DLIN")
-			self.getsongs()
-			click.echo("All done!")
+		if bitrate:
+			bitrate = getBitrateNumberFromText(bitrate)
 
+		self.addToQueue(links, bitrate)
+
+	# change a settings value
 	def change(self,key,value):
 		self.settings[key] = value
+	# reset settings to "default"
 	def resetSetting(self):
 		self.settings = loadSettings(self.configFolder)
+	# print current settings
 	def printSettings(self):
 		print("printing settings")
 		for k, v in self.settings.items():
 			print(f'{k}\n{v}\n\n')
+
+
 if __name__ == '__main__':
-	dl320 = ("downloads320.txt",)
-	dlflac = ("downloadsflac.txt",)
-	tp = DLR(None, ".")
-	tp.printSettings()
-	# tp.download(dl320, "320", ".",)
-	# tp.download(dlflac, "flac", ".", True)
+	tp = DLR(portable=None)
+	
+	tp.loadLinks(url=['https://www.deezer.com/en/track/1','https://www.deezer.com/en/track/2'], bitrate="320")
+	tp.loadLinks(filepath='downloadsflac.txt', bitrate="flac")
+
+	tp.change('downloadLocation','.')
+
+	tp.getsongs()
+
